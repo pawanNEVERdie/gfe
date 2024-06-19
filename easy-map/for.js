aac867e9e12ce1273b84a9dfcce9cb461fecdff4
isafck <insaneesz1e@gmail.com>
2024-06-19 13:05:32 +0530

    diff btewn foreach n map

---
+++ easy-map/for.js
@@ -0,0 +1,5 @@
+const arr =[1,2,4]
+const doubled  =arr.forEach((numer=>{
+
+}))
+console.log(doubled)
---
+++ easy-map/map.js
@@ -0,0 +1,6 @@
+const arr = [1,2,3]
+const doubled  = arr.map((num)=>{
+return num*2
+}
+)
+console.log(doubled)
